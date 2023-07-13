ddfe208da4ddcecfbb26bb1da0be4b9c5e1f56c6
farzinsaniya <saniyafarzin75@gmail.com>
2023-07-13 22:10:48 +0530

    imported pages in App.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
+import Home from "./pages/Home";
+import Customizer from "./pages/Customizer";
+import Canvas from "./canvas";
 
 function App() {
   
   return (
-  <div>
-    <h1 className="head-text" >hello</h1>
-  </div>
+  <main className="app transition-all ease-in" >
+    <Home />
+    <Canvas />
+    <Customizer />
+  </main>
   )
 }
 
